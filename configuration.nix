78cbcb03d68366cdccc27020b849ca6a5b8ba90e
Mark Homoky <25572571+mhomoky@users.noreply.github.com>
2025-06-30 13:42:24 +0100

    installer scripted

---
+++ configuration.nix
@@ -0,0 +1,127 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./disko.nix
+  ];
+
+  networking.hostName = "nixbitcoin-portable";
+  time.timeZone = "Europe/London";
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  users.users.nixbtc = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = [
+      # Add your SSH public key here
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJxbuUFahg5/QCUq56bqfpJeW/hof9RAzgw0XmEOON4F" # id_ed25519_private_cloud.pub
+    ];
+  };
+  security.sudo.wheelNeedsPassword = false;
+
+  services.openssh.enable = true;
+  services.openssh.settings.PasswordAuthentication = false;
+
+  # SOPS secrets
+  sops = {
+    age.keyFile = "/etc/age.key";
+    secrets.tailscale-oauth = {
+      sopsFile = ./secrets/tailscale-oauth.yaml;
+      format = "yaml";
+    };
+  };
+
+  # Tailscale using OAuth client/secret
+  services.tailscale = {
+    enable = true;
+    useRoutingFeatures = "both";
+    extraUpFlags = [
+      "--ssh"
+      "--advertise-exit-node"
+      "--accept-dns=false"
+    ];
+    # Use the decrypted secret
+    authKeyFile = config.sops.secrets.tailscale-oauth.path;
+  };
+
+  # nix-bitcoin stack
+  nix-bitcoin = {
+    enable = true;
+    bitcoin = {
+      enable = true;
+      package = pkgs.bitcoinKnots;
+      extraConfig = ''
+        txindex=1
+        server=1
+        rpcbind=0.0.0.0
+        rpcallowip=192.168.0.0/16
+      '';
+    };
+    lightning = {
+      enable = true;
+      implementation = "clightning";
+    };
+    fulcrum.enable = true;
+  };
+
+  networking.firewall.allowedTCPPorts = [ 8332 9735 50001 50002 22 80 443 ];
+  networking.firewall.allowedUDPPorts = [ ];
+
+  # Tor for external access
+  services.tor.enable = true;
+  services.tor.hiddenServices = {
+    bitcoin = {
+      version = 3;
+      map = [{ port = 8332; target = 8332; }];
+    };
+    lightning = {
+      version = 3;
+      map = [{ port = 9735; target = 9735; }];
+    };
+    fulcrum = {
+      version = 3;
+      map = [{ port = 50002; target = 50002; }];
+    };
+  };
+
+  # Ente Photos (web) via Docker
+  virtualisation.oci-containers.containers = {
+    ente-photos = {
+      image = "enteio/photos:latest";
+      ports = [ "3000:3000" ];
+      environment = {
+        ENTE_CONFIG = "/data/config.yaml";
+      };
+      volumes = [
+        "/var/lib/ente:/data"
+      ];
+      extraOptions = [ "--restart=always" ];
+    };
+  };
+
+  # Caddy with ACME/Let's Encrypt
+  services.caddy = {
+    enable = true;
+    email = "mjhsdc@protonmail.com";
+    virtualHosts."photos.rusty.red" = {
+      extraConfig = ''
+        reverse_proxy localhost:3000
+      '';
+    };
+  };
+
+  # Auto-upgrade
+  system.autoUpgrade = {
+    enable = true;
+    allowReboot = false;
+    flake = "github:yourusername/nixbitcoin-portable";
+  };
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  fileSystems."/".options = [ "noatime" "discard" ]; # Good for SSD and USB
+
+  swapDevices = [ { device = "/swapfile"; size = 4096; } ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+}
---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Portable Bitcoin Knots Node with Lightning, Fulcrum, Tailscale, SOPS, Ente Photos, Caddy, ACME";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nix-bitcoin.url = "github:fort-nix/nix-bitcoin/release";
+    disko.url = "github:nix-community/disko";
+    sops-nix.url = "github:Mic92/sops-nix";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, nix-bitcoin, disko, sops-nix, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in {
+        nixosConfigurations = {
+          nixbitcoin-portable = pkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+              ./configuration.nix
+              nix-bitcoin.nixosModules.default
+              disko.nixosModules.disko
+              sops-nix.nixosModules.sops
+            ];
+          };
+        };
+      });
+}
